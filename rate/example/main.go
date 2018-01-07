407bf6926e70a985ee6b21a7bbcc2260b5c3c41a
kpacha <kpacha666@gmail.com>
2018-01-07 21:50:53 +0100

    use the new krakend 0.4 features

--- rate/example/main.go
+++ rate/example/main.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"os"
 
-	"github.com/devopsfaith/krakend/config/viper"
-	"github.com/devopsfaith/krakend/logging/gologging"
+	"github.com/devopsfaith/krakend/config"
+	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
 	krakendgin "github.com/devopsfaith/krakend/router/gin"
 	"github.com/gin-gonic/gin"
@@ -22,7 +22,7 @@ func main() {
 	configFile := flag.String("c", "/etc/krakend/configuration.json", "Path to the configuration filename")
 	flag.Parse()
 
-	parser := viper.New()
+	parser := config.NewParser()
 	serviceConfig, err := parser.Parse(*configFile)
 	if err != nil {
 		log.Fatal("ERROR:", err.Error())
@@ -32,7 +32,7 @@ func main() {
 		serviceConfig.Port = *port
 	}
 
-	logger, err := gologging.NewLogger(*logLevel, os.Stdout, "[KRAKEND]")
+	logger, err := logging.NewLogger(*logLevel, os.Stdout, "[KRAKEND]")
 	if err != nil {
 		log.Fatal("ERROR:", err.Error())
 	}
