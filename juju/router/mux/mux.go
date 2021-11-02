64fa175099caf1291257d902184f76e65a1eb5fd
Pablo D.A <pablo.diaz.aros@gmail.com>
2021-11-02 17:52:04 +0100

    add error with logging.Logger on juju and rate

--- juju/router/mux/mux.go
+++ juju/router/mux/mux.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
 	"github.com/luraproject/lura/proxy"
 	luramux "github.com/luraproject/lura/router/mux"
 
@@ -14,12 +15,18 @@ import (
 	"github.com/devopsfaith/krakend-ratelimit/juju/router"
 )
 
+// HandlerFactoryWithLogger is the out-of-the-box basic ratelimit handler factory using the default krakend endpoint
+// handler for the mux router
+var HandlerFactoryWithLogger = func(l logging.Logger) luramux.HandlerFactory {
+	return NewRateLimiterMw(luramux.EndpointHandler, l)
+}
+
 // HandlerFactory is the out-of-the-box basic ratelimit handler factory using the default krakend endpoint
 // handler for the mux router
-var HandlerFactory = NewRateLimiterMw(luramux.EndpointHandler)
+var HandlerFactory = NewRateLimiterMw(luramux.EndpointHandler, logging.NoOp)
 
 // NewRateLimiterMw builds a rate limiting wrapper over the received handler factory.
-func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
+func NewRateLimiterMw(next luramux.HandlerFactory, l logging.Logger) luramux.HandlerFactory {
 	return func(remote *config.EndpointConfig, p proxy.Proxy) http.HandlerFunc {
 		handlerFunc := next(remote, p)
 
@@ -29,14 +36,14 @@ func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
 		}
 
 		if cfg.MaxRate > 0 {
-			handlerFunc = NewEndpointRateLimiterMw(juju.NewLimiter(float64(cfg.MaxRate), cfg.MaxRate))(handlerFunc)
+			handlerFunc = NewEndpointRateLimiterMw(juju.NewLimiter(float64(cfg.MaxRate), cfg.MaxRate), l)(handlerFunc)
 		}
 		if cfg.ClientMaxRate > 0 {
 			switch strings.ToLower(cfg.Strategy) {
 			case "ip":
-				handlerFunc = NewIpLimiterWithKeyMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate)(handlerFunc)
+				handlerFunc = NewIpLimiterWithKeyMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate, l)(handlerFunc)
 			case "header":
-				handlerFunc = NewHeaderLimiterMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate)(handlerFunc)
+				handlerFunc = NewHeaderLimiterMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate, l)(handlerFunc)
 			}
 		}
 		return handlerFunc
@@ -47,12 +54,12 @@ func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
 type EndpointMw func(http.HandlerFunc) http.HandlerFunc
 
 // NewEndpointRateLimiterMw creates a simple ratelimiter for a given handlerFunc
-func NewEndpointRateLimiterMw(tb juju.Limiter) EndpointMw {
+func NewEndpointRateLimiterMw(tb juju.Limiter, l logging.Logger) EndpointMw {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			if !tb.Allow() {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusServiceUnavailable)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			next(w, r)
@@ -61,21 +68,21 @@ func NewEndpointRateLimiterMw(tb juju.Limiter) EndpointMw {
 }
 
 // NewHeaderLimiterMw creates a token ratelimiter using the value of a header as a token
-func NewHeaderLimiterMw(header string, maxRate float64, capacity int64) EndpointMw {
-	return NewTokenLimiterMw(HeaderTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity))
+func NewHeaderLimiterMw(header string, maxRate float64, capacity int64, l logging.Logger) EndpointMw {
+	return NewTokenLimiterMw(HeaderTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity), l)
 }
 
 // NewIpLimiterMw creates a token ratelimiter using the IP of the request as a token
-func NewIpLimiterMw(maxRate float64, capacity int64) EndpointMw {
-	return NewTokenLimiterMw(IPTokenExtractor, juju.NewMemoryStore(maxRate, capacity))
+func NewIpLimiterMw(maxRate float64, capacity int64, l logging.Logger) EndpointMw {
+	return NewTokenLimiterMw(IPTokenExtractor, juju.NewMemoryStore(maxRate, capacity), l)
 }
 
 // NewIpLimiterWithKeyMw creates a token ratelimiter using the IP of the request as a token
-func NewIpLimiterWithKeyMw(header string, maxRate float64, capacity int64) EndpointMw {
+func NewIpLimiterWithKeyMw(header string, maxRate float64, capacity int64, l logging.Logger) EndpointMw {
 	if header == "" {
-		return NewIpLimiterMw(maxRate, capacity)
+		return NewIpLimiterMw(maxRate, capacity, l)
 	}
-	return NewTokenLimiterMw(NewIPTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity))
+	return NewTokenLimiterMw(NewIPTokenExtractor(header), juju.NewMemoryStore(maxRate, capacity), l)
 }
 
 // TokenExtractor defines the interface of the functions to use in order to extract a token for each request
@@ -114,18 +121,18 @@ func HeaderTokenExtractor(header string) TokenExtractor {
 }
 
 // NewTokenLimiterMw returns a token based ratelimiting endpoint middleware with the received TokenExtractor and LimiterStore
-func NewTokenLimiterMw(tokenExtractor TokenExtractor, limiterStore krakendrate.LimiterStore) EndpointMw {
+func NewTokenLimiterMw(tokenExtractor TokenExtractor, limiterStore krakendrate.LimiterStore, l logging.Logger) EndpointMw {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			tokenKey := tokenExtractor(r)
 			if tokenKey == "" {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusTooManyRequests)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			if !limiterStore(tokenKey).Allow() {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusTooManyRequests)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			next.ServeHTTP(w, r)
--- rate/router/mux/mux.go
+++ rate/router/mux/mux.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
 	"github.com/luraproject/lura/proxy"
 	luramux "github.com/luraproject/lura/router/mux"
 
@@ -13,12 +14,18 @@ import (
 	"github.com/devopsfaith/krakend-ratelimit/rate/router"
 )
 
+// HandlerFactoryWithLogger is the out-of-the-box basic ratelimit handler factory using the default krakend endpoint and logger
+// handler for the mux router
+var HandlerFactoryWithLogger = func(l logging.Logger) luramux.HandlerFactory {
+	return NewRateLimiterMw(luramux.EndpointHandler, l)
+}
+
 // HandlerFactory is the out-of-the-box basic ratelimit handler factory using the default krakend endpoint
 // handler for the mux router
-var HandlerFactory = NewRateLimiterMw(luramux.EndpointHandler)
+var HandlerFactory = NewRateLimiterMw(luramux.EndpointHandler, logging.NoOp)
 
 // NewRateLimiterMw builds a rate limiting wrapper over the received handler factory.
-func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
+func NewRateLimiterMw(next luramux.HandlerFactory, l logging.Logger) luramux.HandlerFactory {
 	return func(remote *config.EndpointConfig, p proxy.Proxy) http.HandlerFunc {
 		handlerFunc := next(remote, p)
 
@@ -28,14 +35,14 @@ func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
 		}
 
 		if cfg.MaxRate > 0 {
-			handlerFunc = NewEndpointRateLimiterMw(rate.NewLimiter(float64(cfg.MaxRate), cfg.MaxRate))(handlerFunc)
+			handlerFunc = NewEndpointRateLimiterMw(rate.NewLimiter(float64(cfg.MaxRate), cfg.MaxRate), l)(handlerFunc)
 		}
 		if cfg.ClientMaxRate > 0 {
 			switch strings.ToLower(cfg.Strategy) {
 			case "ip":
-				handlerFunc = NewIpLimiterMw(float64(cfg.ClientMaxRate), cfg.ClientMaxRate)(handlerFunc)
+				handlerFunc = NewIpLimiterMw(float64(cfg.ClientMaxRate), cfg.ClientMaxRate, l)(handlerFunc)
 			case "header":
-				handlerFunc = NewHeaderLimiterMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate)(handlerFunc)
+				handlerFunc = NewHeaderLimiterMw(cfg.Key, float64(cfg.ClientMaxRate), cfg.ClientMaxRate, l)(handlerFunc)
 			}
 		}
 		return handlerFunc
@@ -46,12 +53,12 @@ func NewRateLimiterMw(next luramux.HandlerFactory) luramux.HandlerFactory {
 type EndpointMw func(http.HandlerFunc) http.HandlerFunc
 
 // NewEndpointRateLimiterMw creates a simple ratelimiter for a given handlerFunc
-func NewEndpointRateLimiterMw(tb rate.Limiter) EndpointMw {
+func NewEndpointRateLimiterMw(tb rate.Limiter, l logging.Logger) EndpointMw {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			if !tb.Allow() {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusServiceUnavailable)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			next(w, r)
@@ -60,13 +67,13 @@ func NewEndpointRateLimiterMw(tb rate.Limiter) EndpointMw {
 }
 
 // NewHeaderLimiterMw creates a token ratelimiter using the value of a header as a token
-func NewHeaderLimiterMw(header string, maxRate float64, capacity int) EndpointMw {
-	return NewTokenLimiterMw(HeaderTokenExtractor(header), rate.NewMemoryStore(maxRate, capacity))
+func NewHeaderLimiterMw(header string, maxRate float64, capacity int, l logging.Logger) EndpointMw {
+	return NewTokenLimiterMw(HeaderTokenExtractor(header), rate.NewMemoryStore(maxRate, capacity), l)
 }
 
 // NewIpLimiterMw creates a token ratelimiter using the IP of the request as a token
-func NewIpLimiterMw(maxRate float64, capacity int) EndpointMw {
-	return NewTokenLimiterMw(IPTokenExtractor, rate.NewMemoryStore(maxRate, capacity))
+func NewIpLimiterMw(maxRate float64, capacity int, l logging.Logger) EndpointMw {
+	return NewTokenLimiterMw(IPTokenExtractor, rate.NewMemoryStore(maxRate, capacity), l)
 }
 
 // TokenExtractor defines the interface of the functions to use in order to extract a token for each request
@@ -91,18 +98,18 @@ func HeaderTokenExtractor(header string) TokenExtractor {
 }
 
 // NewTokenLimiterMw returns a token based ratelimiting endpoint middleware with the received TokenExtractor and LimiterStore
-func NewTokenLimiterMw(tokenExtractor TokenExtractor, limiterStore krakendrate.LimiterStore) EndpointMw {
+func NewTokenLimiterMw(tokenExtractor TokenExtractor, limiterStore krakendrate.LimiterStore, l logging.Logger) EndpointMw {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			tokenKey := tokenExtractor(r)
 			if tokenKey == "" {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusTooManyRequests)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			if !limiterStore(tokenKey).Allow() {
+				l.Error(krakendrate.ErrLimited)
 				w.WriteHeader(http.StatusTooManyRequests)
-				w.Write([]byte(krakendrate.ErrLimited.Error()))
 				return
 			}
 			next.ServeHTTP(w, r)
